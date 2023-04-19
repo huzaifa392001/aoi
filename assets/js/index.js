2426a888614ace3418cd9294a9a525546456f86a
huzaifaOD <huzaifa.iqbal@olivedigital.com>
2023-04-19 05:12:15 +0500

    day 2 work uploaded

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,258 +1,181 @@
-gsap.registerPlugin(ScrollTrigger);
-
-// // Script for sticky header
-
-// $(window).scroll(function() {
-//     if ($(this).scrollTop() > 1) {
-//         $('header').addClass("sticky");
-//     } else {
-//         $('header').removeClass("sticky");
-//     }
-// });
-
-$(function () {
-    $('.themeBtn')
-        .on('mouseenter', function (e) {
-            var parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-            $(this).find('span').css({top: relY, left: relX})
-        })
-        .on('mouseout', function (e) {
-            var parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-            $(this).find('span').css({top: relY, left: relX})
-        });
-    // $('[href=#]').click(function(){return false});
-});
-
-// Page RoleBack to Top
-$(document).ready(function () {
-    $('.roleback').click(function () {
-        $("html, body").animate({scrollTop: 0}, 800);
-        return false;
-    });
-});
-
-// Go Down
-$(function () {
-    $('.goDown').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $(".aboutSec").offset().top
-        }, 500, 'linear');
-    });
-});
-
-
-// Script for AOS Animation Initialize
-// AOS.init();
-
-
-// MAIN SLIDER
-function mainSlider() {
-    var interleaveOffset = 0.5;
-    var swiperOptions = {
-        loop: true,
-        speed: 1000,
-        parallax: true,
-        // autoplay: {
-        //     delay: 3500,
-        //     disableOnInteraction: false,
-        // },
-        autoplay: false,
-        grabCursor: false,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-            // type: "fraction",
-        },
-        navigation: {
-            nextEl: ".swiper-next",
-            prevEl: ".swiper-prev",
-        },
-        on: {
-            progress: function () {
-                var swiper = this;
-                for (var i = 0; i < swiper.slides.length; i++) {
-                    var slideProgress = swiper.slides[i].progress;
-                    var innerOffset = swiper.width * interleaveOffset;
-                    var innerTranslate = slideProgress * innerOffset;
-                    swiper.slides[i].querySelector(".slide-inner").style.transform = "translate3d(" + innerTranslate + "px, 0, 0)";
-                }
-            },
-            touchStart: function () {
-                var swiper = this;
-                for (var i = 0; i < swiper.slides.length; i++) {
-                    swiper.slides[i].style.transition = "";
-                }
-            },
-            setTransition: function (speed) {
-                var swiper = this;
-                for (var i = 0; i < swiper.slides.length; i++) {
-                    swiper.slides[i].style.transition = speed + "ms";
-                    swiper.slides[i].querySelector(".slide-inner").style.transition = speed + "ms";
-                }
-            },
-        },
-    };
-
-    const swiper = new Swiper(".homeSlider", swiperOptions);
-
-    // DATA BACKGROUND IMAGE
-    var pageSection = $(".bg-image");
-    pageSection.each(function (indx) {
-        if ($(this).attr("data-background")) {
-            $(this).css("background-image", "url(" + $(this).data("background") + ")");
-        }
-    });
-}
-
-mainSlider();
-
-
-//gsap loader
-const tl = gsap.timeline({duration: 0.10});
-
-function preLoader(tl) {
-    tl.to('.preLoader.black > img',
-        {
-            delay: 1,
-            y: 50,
-            autoAlpha: 0,
-        }
-    )
-        .to('.preLoader.black',
-            {
-                yPercent: -100,
-            },
-        )
-        .to('.preLoader.white',
-            {
-                yPercent: -100,
-            }
-        )
-        .to(".preLoader",
-            {
-                css: {
-                    display: 'none'
-                }
-            }
-        )
-}
-
 // function initialization
-$(window).on('load', function () {
-    preLoader(tl);
+$(window).on("load", function () {
+    $(window).scrollTop(0);
 });
 
+$(function () {
+    $(window).scrollTop(0);
+    gsap.registerPlugin(ScrollTrigger);
+    $('[data-magnetic]').each(function () {
+        new Magnetic(this);
+    });
+    allFunctionInit();
+});
 
-// Swiper Slider for Products or Testimonials
+function allFunctionInit() {
+    lenisSetup();
+    // heroAnim();
+    // progressBar();
+    customSlider();
+}
 
-function reviewSlider() {
-    var reviewBox = new Swiper(".reviewSlider", {
-        slidesPerView: 1,
-        loop: true,
-        spaceBetween: 25,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        // pagination: {
-        //     el: ".swiper-bullets",
-        //     clickable: true,
-        //     // type: "fraction",
-        // },
-        responsive: [
-            {
-                breakpoint: 1199,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    arrows: false
-                }
-            },
-            {
-                breakpoint: 575,
-                settings: {
-                    slidesToShow: 1,
-                    arrows: false
-                }
-            }]
+function lenisSetup() {
+    const lenis = new Lenis({
+        duration: 1.5,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+        smooth: true,
+        mouseMultiplier: 1,
+    })
+    lenis.scrollTo(document.querySelector('#home'))
+
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf);
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+            lenis.scrollTo(this.getAttribute("href"));
+        });
     });
 }
 
-$(function () {
-    reviewSlider();
-});
+function preloader() {
+    let loadertl = gsap.timeline({onComplete: scrollLetters()});
 
+    loadertl
+        .to('.loadingScreen', {
+            delay: 4,
+            autoAlpha: 0
+        })
+        .to(".preloader div", {
+            yPercent: -100,
+            stagger: 0.2,
+            ease: 'Power2.out'
+        })
+        .to('.bannerSec .content h1 span .char', {
+            x: 0,
+            autoAlpha: 1,
+            duration: 0.3,
+            stagger: 0.1,
+            scale: 1,
+            ease: "circ.out",
+        })
+        .add(function () {
+            buttonUnderlineAnim.setDirection(1);
+            buttonUnderlineAnim.play();
+        }, '<')
+}
 
-// Script for Slick Carousel
-// $('.reviewsCarousel').slick({
-//     autoplay: true,
-//     autoplaySpeed: 5000,
-//     dots: false,
-//     arrows: true,
-//     infinite: true,
-//     slidesToShow: 4,
-//     slidesToScroll: 1,
-//     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-double-left"></i></button>',
-//     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-double-right"></i></button>',
-//     responsive: [{
-//         breakpoint: 1199,
-//         settings: {
-//             slidesToShow: 3
-//         }
-//     },
-//     {
-//         breakpoint: 768,
-//         settings: {
-//             slidesToShow: 2,
-//             arrows: false
-//         }
-//     },
-//     {
-//         breakpoint: 575,
-//         settings: {
-//             slidesToShow: 1,
-//             arrows: false
-//         }
-//     }
-//     ]
-// });
+function heroAnim() {
+    let heroTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.heroSec',
+            scrub: 1,
+            pin: true,
+            pinContainer: false
+        }
+    })
+    heroTl
+        .to('.heroSec .mainHeading', {autoAlpha: 0,})
+        .to('.heroSec .mainImg', {scale: 1.25}, "<")
+        .from(".heroSec .content p", {autoAlpha: 0, y: 100})
+        .from(".heroSec .content .verticleLine", {scaleY: 0})
+        .from(".heroSec .content .verticleLine", {scaleY: 0})
+        .to('.heroSec .mainImg', {autoAlpha: 0}, "<")
+}
 
+function progressBar() {
+    let sections = document.querySelectorAll('section')
+    let bar = document.querySelector(".progressBar")
+    let pixels = window.pageYOffset;
+    let pageHeight = document.body.scrollHeight - window.innerHeight;
+    let progress = (100 * pixels) / pageHeight;
+    for (i = 1; i <= sections.length; i++) {
+        bar.innerHTML += '<span></span>';
+    }
+    document.addEventListener("scroll", function () {
+        document.querySelector(".progress").style.width = progress + "%";
+    });
+    // window.addEventListener("scroll", function() {
+    //     // Calculate the position of each section
+    //     let sectionPositions = [];
+    //     sections.forEach(function(sectionId) {
+    //         let section = document.getElementById(sectionId);
+    //         let position = section.offsetTop;
+    //         sectionPositions.push(position);
+    //     });
+    //
+    //     // Calculate the percentage of the page that has been scrolled
+    //     let scrollTop = window.scrollY;
+    //     let docHeight = document.body.offsetHeight;
+    //     let winHeight = window.innerHeight;
+    //     let scrollPercent = scrollTop / (docHeight - winHeight) * 100;
+    //
+    //     // Update the width of the progress bar
+    //     let progressBar = document.querySelector(".progress-bar");
+    //     progressBar.style.width = scrollPercent + "%";
+    //
+    //     // Highlight the corresponding checkpoint
+    //     let activeIndex = getActiveIndex(scrollTop, sectionPositions);
+    //     highlightCheckpoint(activeIndex);
+    // });
+    //
+    // function getActiveIndex(scrollTop, sectionPositions) {
+    //     for (let i = sectionPositions.length - 1; i >= 0; i--) {
+    //         if (scrollTop >= sectionPositions[i]) {
+    //             return i;
+    //         }
+    //     }
+    //     return 0;
+    // }
+    //
+    // function highlightCheckpoint(index) {
+    //     let checkpoints = document.querySelectorAll(".checkpoint");
+    //     for (let i = 0; i < checkpoints.length; i++) {
+    //         if (i == index) {
+    //             checkpoints[i].classList.add("active");
+    //         } else {
+    //             checkpoints[i].classList.remove("active");
+    //         }
+    //     }
+    // }
 
-// Image Reveal
-// function imgRevealer() {
-//     let revealContainers = document.querySelectorAll(".reveal");
-//     revealContainers.forEach((container) => {
-//         let image = container.querySelector("img");
-//         let revelerTl = gsap.timeline({
-//             scrollTrigger: {
-//                 // UnComment this if you're using Locomotive
-//                 // scroller: '[data-scroll-container]',
-//                 trigger: container,
-//                 toggleActions: "restart none none reset",
-//             }
-//         });
+}
 
-//         revelerTl.set(container, { autoAlpha: 1 });
-//         revelerTl.from(container, .75, {
-//             xPercent: -100,
-//             ease: Power2.out
-//         });
-//         revelerTl.from(image, .75, {
-//             xPercent: 100,
-//             scale: 1.3,
-//             delay: -.75,
-//             ease: Power2.out
-//         });
-//     });
-// }
-// imgRevealer();
+function customSlider() {
+    let card = $('.slide');
+    let lastCard = $(".slideContainer .slide").length - 1;
+    let nextBtn = document.querySelector(".carouselSlider .next")
+    let prevBtn = document.querySelector(".carouselSlider .prev")
+
+    $(nextBtn).on("click", function () {
+        let prependList = function () {
+            if (card.hasClass('activeNow')) {
+                let $slicedCard = card.slice(lastCard).removeClass('transformThis activeNow');
+                $('ul').prepend($slicedCard);
+            }
+        }
+        $('li').last().removeClass('transformPrev').addClass('transformThis').prev().addClass('activeNow');
+        setTimeout(function () {
+            prependList();
+        }, 150);
+    });
+
+    $(prevBtn).on("click", function () {
+        let appendToList = function () {
+            if (card.hasClass('activeNow')) {
+                let $slicedCard = card.slice(0, 1).addClass('transformPrev');
+                $('.carouselSlider').append($slicedCard);
+            }
+        }
+
+        $('li').removeClass('transformPrev').last().addClass('activeNow').prevAll().removeClass('activeNow');
+        setTimeout(function () {
+            appendToList();
+        }, 150);
+    });
+
+}
